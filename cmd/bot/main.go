97b17ea0bebb4beac2efe2b4e6785ba37b06ebbd
Artur Latypov <arlatypov@ozon.ru>
2022-09-26 00:43:19 +0400

    Extract token from ENV

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"log"
+	"os"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
 )
 
-const token = "5767027445:AAF274u3KTgjTJnLC56lR_iIYDl-WJrErHw"
-
 func main() {
+	godotenv.Load()
+
+	token := os.Getenv("TOKEN")
+
 	bot, err := tgbotapi.NewBotAPI(token)
 	if err != nil {
 		log.Panic(err)
